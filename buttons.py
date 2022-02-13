f0a14d971a3012002b2291b8b543b384a6a3289d
Serhii Sliadniev <88987046+ssliadniev@users.noreply.github.com>
2022-02-13 20:57:38 +0200

    add direction.setter for HallButton class

--- buttons.py
+++ buttons.py
@@ -1,4 +1,11 @@
 class Button:
+    """
+    A class that represents Button objects in a button hall\elevator panels.
+
+    parameters:
+    button_name [str]: unique name of button
+    """
+
     def __init__(self, button_name: str):
         self._button_name = button_name
         self.__on = False
@@ -18,12 +25,43 @@ class Button:
 
 
 class ElevatorButton(Button):
-    def __init__(self, button_name: str, floor: int):
+    """
+    A class that represents Button objects in a button elevator panels.
+    This class is inherited from the Button class.
+
+    parameters:
+    button_name [str]: unique name of button
+    floor_number [int]: building floor number
+    """
+
+    def __init__(self, button_name: str, floor_number: int):
         super(ElevatorButton, self).__init__(button_name)
-        self._floor = floor
+        self._floor_number = floor_number
 
 
 class HallButton(Button):
+    """
+    A class that represents Button objects in a button hall panels.
+    This class is inherited from the Button class.
+
+    parameters:
+    button_name [str]: unique name of button
+    floor [int]: building floor number
+    """
+
     def __init__(self, button_name: str, direction: str):
         super(HallButton, self).__init__(button_name)
+        self._direction = direction
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, direction):
+        available_directions = ("up", "down")
+        if direction.upper() not in available_directions:
+            raise ValueError(
+                f"Error. Set wrong direction: '{direction}'.\nAvailable directions: up, down."
+            )
         self._direction = direction
